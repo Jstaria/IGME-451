0ed1db8bf0675a17d7839f55ecd0b712fe1f3477
Jstaria <jws8554@g.rit.edu>
2025-12-03 10:54:20 -0500

    15. Started Project3

---
+++ Project3/Source.asm
@@ -0,0 +1,21 @@
+INCLUDELIB kernel32.lib
+ExitProcess PROTO
+
+.DATA
+var QWORD 100
+
+.CODE
+; Assign stack allocation
+
+main PROC
+	XOR RCX, RCX
+	XOR RDX, RDX
+	MOV RCX, 33
+	MOV RDX, RCX
+	MOV RCX, var
+	MOV var, RDX
+
+CALL ExitProcess
+main ENDP
+
+END
